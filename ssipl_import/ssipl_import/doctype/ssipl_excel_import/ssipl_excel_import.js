55c8a38461c773d7f5dc0f8dc9b61538bc37090e
Laxman <laxmantandon@gmail.com>
2020-08-09 17:33:08 +0000

    multiple files bug

--- ssipl_import/ssipl_import/doctype/ssipl_excel_import/ssipl_excel_import.js
+++ ssipl_import/ssipl_import/doctype/ssipl_excel_import/ssipl_excel_import.js
@@ -15,5 +15,49 @@ frappe.ui.form.on('SSIPL Excel Import', {
 		}, () => {
 			// call back do somthing here
 		});
+	},
+	start_import_2: function(frm) {
+		if(!frm.doc.excel_file_2) {
+			frappe.throw(__("Please select excel sheet"));
+		}
+
+		frm.call("start_import", {
+			excel_file: frm.doc.excel_file_2
+		}, () => {
+			// call back do somthing here
+		});
+	},
+	start_import_3: function(frm) {
+		if(!frm.doc.excel_file_3) {
+			frappe.throw(__("Please select excel sheet"));
+		}
+
+		frm.call("start_import", {
+			excel_file: frm.doc.excel_file_3
+		}, () => {
+			// call back do somthing here
+		});
+	},
+	start_import_4: function(frm) {
+		if(!frm.doc.excel_file_4) {
+			frappe.throw(__("Please select excel sheet"));
+		}
+
+		frm.call("start_import", {
+			excel_file: frm.doc.excel_file_4
+		}, () => {
+			// call back do somthing here
+		});
+	},
+	start_import_5: function(frm) {
+		if(!frm.doc.excel_file_5) {
+			frappe.throw(__("Please select excel sheet"));
+		}
+
+		frm.call("start_import", {
+			excel_file: frm.doc.excel_file_5
+		}, () => {
+			// call back do somthing here
+		});
 	}
 });
